b91cb388d386cb72fd0faf430d1f4147b4fb2d36
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-11-03 11:31:42 +0530

    welcome

--- Program.cs
+++ Program.cs
@@ -1,43 +1,43 @@
-﻿namespace InsertionSort
+namespace HashTable
 {
-    class program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            int[] arr = new int[] { 23, 9, 3 };
-            int n = 3, i, j, val, flag;
-            for(i= 0; i < n; i++)
-            {
-                val= arr[i];
-                flag = 0;
-                for (j=i-1; j>=0 && flag!=1;)
-                {
-                    if (val < arr[j])
-                    {
-                        arr[j + 1] = arr[j];
-                        j--;
-                        arr[j + 1] = val;
-                    }
-                    else flag = 1;
-                }
-            }
-            foreach(int ele in arr)
-            {
-                Console.WriteLine(ele);
-            }
+            Console.WriteLine("Wellcome to Hash Table and Binary Search Tree");
+
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            hash.Remove("0");
+            hash.Remove("1");
+            hash.Remove("2");
+            hash.Remove("3");
+            hash.Remove("4");
+            hash.Remove("5");
+            hash.Remove("6");
+
+
+            string hash0 = hash.Get("0");
+            Console.WriteLine("0th index value: " + hash0);
+            string hash1 = hash.Get("1");
+            Console.WriteLine("1st index value: " + hash1);
+            string hash2 = hash.Get("2");
+            Console.WriteLine("2nd index value: " + hash2);
+            string hash3 = hash.Get("3");
+            Console.WriteLine("3rd index value: " + hash3);
+            string hash4 = hash.Get("4");
+            Console.WriteLine("4th index value: " + hash4);
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value: " + hash5);
+
+
 
         }
-    }
-    }
-        string str1 = "heart";
-        string str2 = "earth";
-
-        char[] chr1 = str1.ToLower().ToCharArray();
-        char[] chr2 = str2.ToLower().ToCharArray();
-       
-   Array.Sort(chr1);
-   Array.Sort(chr2);
-
-    string val1 = 
     }
 }
