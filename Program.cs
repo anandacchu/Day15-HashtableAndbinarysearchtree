e9b99c8c7786e06a19cd59b8b84fbafc41f6e780
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-11-03 11:22:07 +0530

    welcome

--- Program.cs
+++ Program.cs
@@ -1,31 +1,17 @@
-namespace BinarySearchTree
+namespace HashTable
 {
-    class program
+    public class Program
     {
         public static void Main(string[] args)
         {
-           
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
 
-            int[] arr = { 70, 30, 42, 55, 10 };
-            int temp;
-            for(int j=0;j<=arr.Length-2;j++)
-            {
-                for(int i=0;i<=arr.Length-2;i++)
-                {
-                    if (arr[i] > arr[i+1])
-                    {
-                        temp = arr[i + 1];
-                        arr[i+1] = arr[i];
-                        arr[i] = temp;
-                    }
-                }
-            }
+            hash.GetFreq();
 
-            Console.WriteLine("Sorted Array");
-            foreach (int ele in arr)
-            {
-                Console.Write(ele + " ");
-            }
+
+
+            Console.ReadKey();
+
         }
     }
 }
