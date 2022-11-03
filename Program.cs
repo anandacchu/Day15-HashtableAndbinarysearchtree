3180abdc3aae681a5c046795f68808f7cee8d399
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-11-03 11:25:12 +0530

    welcome

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HashTable
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+
+            hash.GetFreq();
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
